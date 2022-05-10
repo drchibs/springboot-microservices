eb9e13382de2f4e842f4f57fb8a3ec33a097b56c
drchibs <richarddennis789@gmail.com>
2022-05-10 07:26:42 +0100

    Spring Sleuth Added

--- Clients/src/main/java/com/chibs/clients/notification/NotificationDTO.java
+++ Clients/src/main/java/com/chibs/clients/notification/NotificationDTO.java
@@ -1,10 +1,15 @@
 package com.chibs.clients.notification;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class NotificationDTO {
     private String title;
     private String message;
--- Fraud/src/main/java/com/chibs/Fraud/FraudCheckHistoryService.java
+++ Fraud/src/main/java/com/chibs/Fraud/FraudCheckHistoryService.java
@@ -1,10 +1,12 @@
 package com.chibs.Fraud;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class FraudCheckHistoryService {
 
     private final FraudCheckHistoryRepository fraudCheckHistoryRepository;
@@ -16,6 +18,7 @@ public class FraudCheckHistoryService {
                         .customerId(customerId)
                 .build()
         );
+        log.info("Customer {} is not fraudulent", customerId);
         return false;
     }
 }
